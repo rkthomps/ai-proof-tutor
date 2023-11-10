321884e1fc189440af56b22f1068fde66d281a1d
rkthomps <rkthomps@calpoly.edu>
2023-11-09 17:13:20 -0800

    Don't use secrets file. Use env keys

--- src/tutor/query_openai.py
+++ src/tutor/query_openai.py
@@ -28,33 +28,13 @@ def get_gpt4_response(
 
 
 def get_client() -> OpenAI:
-    if not os.path.exists(SECRETS_PATH):
-        raise FileNotFoundError(
-            (
-                f"Could not find {SECRETS_PATH}. Make sure you are in the "
-                f"root project directory, and that you have a {SECRETS_PATH} file "
-                "with your org key and api key."
-            )
-        )
+    api_env_key = "OPENAI_API_KEY"
+    if not api_env_key in os.environ:
+        raise KeyError(f"You must set the environment variable '{api_env_key}'")
+    api_key = os.environ[api_env_key]
 
-    with open(SECRETS_PATH, "r") as fin:
-        conf = load(fin, Loader=Loader)
-
-    if not "api_key" in conf:
-        raise KeyError(
-            (
-                f"Your {SECRETS_PATH} file must have your open ai "
-                'api key under "api_key".'
-            )
-        )
-    api_key: str = conf["api_key"]
-
-    if not "org_key" in conf:
-        raise KeyError(
-            (
-                f"Your {SECRETS_PATH} file must have your open ai "
-                'org key under "org_key".'
-            )
-        )
-    org_key: str = conf["org_key"]
+    org_env_key = "OPENAI_ORG_KEY"
+    if not org_env_key in os.environ:
+        raise KeyError(f"You must set the environment variable '{org_env_key}'")
+    org_key = os.environ[org_env_key]
     return OpenAI(api_key=api_key.strip(), organization=org_key.strip())
